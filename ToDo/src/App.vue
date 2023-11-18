a6490f1e15baabcb0a5cdb1aa9b770ffb8ac99e2
Alireza Hatami <itshatami@gmail.com>
2023-11-18 23:21:20 +0330

    first initial

---
+++ ToDo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-emerald-300" style="max-width: 400px;">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ ToDo/src/components/ListComponent.vue
@@ -0,0 +1,18 @@
+<template>
+   <li>
+
+   </li>
+</template>
+
+<script>
+export default {
+   setup () {
+      
+      return {}
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
